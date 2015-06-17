bf19ad117a6406a7a4faef8897edde443931b037
jwells <jameswells081@air-watch.com>
2015-06-17 11:40:03 -0400

    Initial Commit

---
+++ octree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  octree
+//
+//  Created by James Wells on 6/17/15.
+//  Copyright (c) 2015 James Wells. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
